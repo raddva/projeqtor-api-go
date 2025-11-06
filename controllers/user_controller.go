9b307f06e5882b49a6d598e81358587226fff5af
raddva <111165508+raddva@users.noreply.github.com>
2025-11-06 08:01:54 +0700

    feat: Register #6

---
+++ controllers/user_controller.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/copier"
+	"github.com/raddva/projeqtor-api-go/models"
+	"github.com/raddva/projeqtor-api-go/services"
+	"github.com/raddva/projeqtor-api-go/utils"
+)
+
+type UserController struct {
+	service services.UserService
+}
+
+func NewUserController(s services.UserService) *UserController {
+	return &UserController{service: s}
+}
+
+func (c *UserController) Register(ctx *fiber.Ctx) error {
+	user := new(models.User)
+
+	if err := ctx.BodyParser(user); err != nil {
+		return utils.BadRequest(ctx, "Gagal Parsing Data", err.Error())
+	}
+
+	if err := c.service.Register(user); err != nil {
+		return utils.BadRequest(ctx, "Registrasi Gagal", err.Error())
+	}
+
+	var userResp models.UserResponse
+	_ = copier.Copy(&userResp, &user)
+	return utils.Success(ctx, "Register Success", userResp)
+}
