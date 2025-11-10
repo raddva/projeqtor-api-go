a1f52f637becbf5941addb280335856cc966d5b8
raddva <111165508+raddva@users.noreply.github.com>
2025-11-10 21:47:01 +0700

    feat: Get User Pagination #1

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"strings"
+
 	"github.com/raddva/projeqtor-api-go/config"
 	"github.com/raddva/projeqtor-api-go/models"
 )
@@ -38,4 +40,42 @@ func (r *userRepository) FindByPublicID(publicID string) (*models.User, error) {
 	var user models.User
 	err := config.DB.Where("public_id = ?", publicID).First(&user).Error
 	return &user, err
+}
+
+func (r *userRepository) FindAllPaginated(filter, sort string, limit, offset int)([]models.User, int64, error){
+	var users []models.User
+	var total int64
+
+	db := config.DB.Model(&models.User{})
+
+	// Filter
+	if filter != "" {
+		filterPattern := "%" + filter + "%"
+		db = db.Where("name ILIKE ? OR email ILIKE ?", filterPattern, filterPattern)
+	}
+
+	// Count total
+	if err := db.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	// Sorting
+	if sort != "" {
+		if sort == "-id"{
+			sort = "-internal_id"
+		} else if sort == "id"{
+			sort = "internal_id"
+		}
+
+		if strings.HasPrefix(sort, "-") {
+			sort = strings.TrimPrefix(sort, "-") + " DESC"
+		} else {
+			sort += " ASC"
+		}
+
+		db = db.Order(sort)
+	}
+
+	err := db.Limit(limit).Offset(offset).Find(&users).Error
+	return users, total, err
 }
--- routes/route.go
+++ routes/route.go
@@ -31,5 +31,6 @@ func Setup(app *fiber.App, uc *controllers.UserController){
 
 	userGroup := api.Group("/users")
 	userGroup.Get("/:id", uc.GetUser)
+	userGroup.Get("/page", uc.GetUser)
 
 }
